b8c47901fbbc1f433faa6b3d1632a5ca90a90563
Marco Hernandez <marcohern@gmail.com>
2019-07-28 13:16:48 -0500

    base slugifier

--- src/Lib/BaseSlugifier.php
+++ src/Lib/BaseSlugifier.php
@@ -3,16 +3,65 @@
 namespace Marcohern\Slugifier\Lib;
 
 use Marcohern\Slugifier\Lib\SlugFormatter;
+use Marcohern\Slugifier\Slug;
 
-abstract class BaseSlugifier {
+class BaseSlugifier {
 
   protected $formatter;
 
-  abstract public function checkSlug(string $slug, string $entity='', string $format='%slug-%n', string $formatIfZero='%slug');
-  abstract public function storeSlug(string $slug, string $entity='', string $format='%slug-%n', string $formatIfZero='%slug');
-  abstract public function contextualizeSlug(
-    string $slug, string $entity='',
-    array $slugFormats=[], array $fields=[],
-    string $format='%slug-%n', string $formatIfZero='%slug'
-  );
+  public function __construct() {
+    $this->formatter = new SlugFormatter;
+  }
+
+  public function checkSlug(string $slug, string $entity='') {
+    $slugRecord = Slug::select()->where('entity','=', $entity)->where('slug','=', $slug)->first();
+    if (!$slugRecord) $sequence = 0;
+    else $sequence = $slugRecord->sequence;
+    $slugFormatted = $this->formatter->format($slug, $sequence);
+    return [
+      'entity' => $entity,
+      'slug' => $slugFormatted,
+      'sequence' => $sequence
+    ];
+  }
+
+  public function storeSlug(string $slug, string $entity='') {
+    $slugRecord = Slug::select()->where('entity','=', $entity)->where('slug','=', $slug)->first();
+    $sequence = 0;
+    if ($slugRecord) {
+      $sequence = $slugRecord->sequence;
+      $slugRecord->sequence++;
+    } else {
+      $slugRecord = new Slug;
+      $slugRecord->entity = $entity;
+      $slugRecord->slug = $slug;
+      $slugRecord->sequence = $sequence+1;
+    }
+    $slugRecord->save();
+    $formattedSlug = $this->formatter->format($slug, $sequence);
+    return [
+      'entity' => $entity,
+      'slug' => $formattedSlug,
+      'sequence' => $sequence
+    ];
+  }
+
+  public function slugFormat($slug, $format, &$fields=[]) {
+    $result = $format;
+    foreach ($fields as $name => $value) {
+      $result = str_replace("%$name", $value, $result);
+    }
+    $result = str_replace('%slug', $slug, $result);
+    return $result;
+  }
+
+  public function contextualizeSlug(string $slug, string $entity='', array $slugFormats=[], array $fields=[]) {
+    $contextSlug = null;
+    foreach ($slugFormats as $slugFormat) {
+      $contextSlug = $this->slugFormat($slug, $slugFormat, $fields);
+      $slugRecord = Slug::select()->where('entity','=', $entity)->where('slug','=', $contextSlug)->first();
+      if (!$slugRecord) break;
+    }
+    return $this->storeSlug($contextSlug, $entity);
+  }
 }
---
+++ tests/Unit/BaseSlugifierTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Marcohern\Slugifier\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Marcohern\Slugifier\Lib\SlugFormatter;
+use Marcohern\Slugifier\Lib\BaseSlugifier;
+use Marcohern\Slugifier\Slug;
+
+class BaseSlugifierTest extends TestCase
+{
+  use RefreshDatabase;
+  
+  protected function setUp(): void {
+    parent::setUp();
+  }
+
+  protected function tearDown(): void {
+    parent::tearDown();
+  }
+
+  public function test_checkSlug() {
+    $sr = new Slug;
+    $sr->entity = 'actors';
+    $sr->slug = 'brad-pitt';
+    $sr->sequence = 12;
+    $sr->save();
+
+    $slugifier = new BaseSlugifier;
+    $result = $slugifier->checkSlug('brad-pitt', 'actors');
+    $this->assertEquals($result, ['entity' => 'actors','slug' => 'brad-pitt-12','sequence' => 12]);
+  }
+
+  public function test_storeSlug() {
+    $sr = new Slug;
+    $sr->entity = 'actors';
+    $sr->slug = 'brad-pitt';
+    $sr->sequence = 5;
+    $sr->save();
+
+    $slugifier = new BaseSlugifier;
+    $result = $slugifier->storeSlug('brad-pitt', 'actors');
+    $this->assertEquals($result, ['entity' => 'actors','slug' => 'brad-pitt-5','sequence' => 5]);
+
+    $sr = Slug::select()->where('entity','=', 'actors')->where('slug', '=', 'brad-pitt')->first();
+    $this->assertEquals($sr->sequence, 6);
+  }
+
+  public function test_contextualizeSlug() {
+    $fields = [
+      'prox' => 'andino',
+      'city' => 'bogota',
+      'zone' => 'chico'
+    ];
+
+    $formats = [
+      '%slug',
+      '%slug-%prox',
+      '%slug-%zone',
+      '%slug-%prox-%city',
+      '%slug-%zone-%city',
+      '%slug-%prox-%zone-%city',
+    ];
+
+    $slugifier = new BaseSlugifier;
+    $r1 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+    $r2 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+    $r3 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+    $r4 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+    $r5 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+    $r6 = (object)$slugifier->contextualizeSlug('bbc', 'bars', $formats, $fields);
+
+    $this->assertEquals($r1->slug, 'bbc');
+    $this->assertEquals($r2->slug, 'bbc-andino');
+    $this->assertEquals($r3->slug, 'bbc-chico');
+    $this->assertEquals($r4->slug, 'bbc-andino-bogota');
+    $this->assertEquals($r5->slug, 'bbc-chico-bogota');
+    $this->assertEquals($r6->slug, 'bbc-andino-chico-bogota');
+  }
+}
